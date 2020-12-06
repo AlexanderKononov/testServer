7459c203051ee69f3876a67b52e4a324c50d3cf7
AlexanderKononov <Kononov.nov@mail.ru>
2020-12-06 21:21:01 +0000

    fix init

--- init.sh
+++ init.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
+mkdir /home/box/web/
+mkdir /home/box/web/etc/
+cp ./* /home/box/web/etc/
 
 sudo ﻿ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
+
 #sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
 #sudo /etc/init.d/gunicorn restart
 ﻿#sudo /etc/init.d/mysql start﻿
