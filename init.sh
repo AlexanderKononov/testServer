8fe3fde4a6c6ab55652114818c81903e75187087
AlexanderKononov <Kononov.nov@mail.ru>
2020-12-06 22:02:06 +0000

    reorganase sites-enabled

--- init.sh
+++ init.sh
@@ -2,7 +2,7 @@
 mkdir /home/box/web/
 cp -r ./* /home/box/web/
 
-sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+sudo ln -s /home/box/web/etc/nginx/sites-enabled/test.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 
 #sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
