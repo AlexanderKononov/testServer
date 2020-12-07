6297a80f66faf5c323f0e86f0b5f14dd2c8bf2e5
AlexanderKononov <Kononov.nov@mail.ru>
2020-12-07 11:22:00 +0000

    reorganise again

--- init.sh
+++ init.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+mkdir /home/box/web/
+cp -r ./* /home/box/web/
 
-sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
+sudo ln -sf /home/box/web/etc/test.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
