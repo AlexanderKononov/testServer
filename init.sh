716b5104df2ac143b213398462ccb9e1fc5271d8
AlexanderKononov <Kononov.nov@mail.ru>
2020-12-07 10:49:52 +0000

    sheng initsh

--- init.sh
+++ init.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 
-mkdir /home/box/web/
-cp -r ./* /home/box/web/
-
-sudo rm -rf /etc/nginx/sites-enabled/default
-sudo ln -sf ./etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
