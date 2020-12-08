90118e43b54ee9035d69456218f04a84da146caf
AlexanderKononov <kononov.nov@mail.ru>
2020-12-09 02:40:08 +0700

    proxy

---
+++ hello.py
@@ -0,0 +1,10 @@
+def app(environ, start_response):
+    """Simplest possible application object"""
+    data = ''.join(format(ord(i), 'b') for i in environ['QUERY_STRING'].replace('&', '\n'))
+    status = '200 OK'
+    response_headers = [
+        ('Content-type', 'text/plain'),
+        ('Content-Length', str(len(data)))
+    ]
+    start_response(status, response_headers)
+    return iter([data])
--- init.sh
+++ init.sh
@@ -2,5 +2,10 @@
 mkdir /home/box/web/
 cp -r ./* /home/box/web/
 
-sudo ln -sf /home/box/web/etc/test.conf /etc/nginx/sites-enabled/default
+#sudo ln -sf /home/box/web/etc/test.conf /etc/nginx/sites-enabled/default
+#sudo /etc/init.d/nginx restart
+sudo ln -s /home/box/web/etc/test.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
+sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
+sudo /etc/init.d/gunicorn restart
+#sudo /etc/init.d/mysql start﻿
