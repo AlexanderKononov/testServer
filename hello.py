f9e731723ad108bb316aaeb44a74871f03512674
AlexanderKononov <kononov.nov@mail.ru>
2020-12-09 06:44:48 +0700

    fix wsgi code

--- hello.py
+++ hello.py
@@ -1,9 +1,9 @@
 def hello_app(environ, start_response):
-    data = ''.join(format(ord(i), 'b') for i in environ['QUERY_STRING'].replace('&', '\n'))
+    body = [bytes(i + '\n', 'ascii') for i in environ['QUERY_STRING'].split('&')]
     status = '200 OK'
     response_headers = [
         ('Content-type', 'text/plain'),
-        ('Content-Length', str(len(data)))
+        ('Content-Length', str(len(body)))
     ]
     start_response(status, response_headers)
-    return iter([data])
+    return body
--- init.sh
+++ init.sh
@@ -8,7 +8,7 @@ sudo ln -sf /home/box/web/etc/test.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
 cd /home/box/web/
-#sudo gunicorn --bind='0.0.0.0:8080' hello.py:hello_app
+sudo gunicorn --bind='0.0.0.0:8080' hello:hello_app &
 #sudo gunicorn -c /etc/gunicorn.d/hello.conf.py /home/box/web/hello.py:hello_app
-sudo /etc/init.d/gunicorn restart hello.py:hello_app
+#sudo /etc/init.d/gunicorn restart hello:hello_app
 #sudo /etc/init.d/mysql start
