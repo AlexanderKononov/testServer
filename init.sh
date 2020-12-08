2c7baf4dc3b4d45db238c9d8178b6f2493759b60
AlexanderKononov <kononov.nov@mail.ru>
2020-12-09 04:58:07 +0700

    gunicorn fix run

--- init.sh
+++ init.sh
@@ -8,5 +8,6 @@ sudo rm /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/test.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/etc/hello.conf.py /etc/gunicorn.d/hello.conf.py
-sudo /etc/init.d/gunicorn -c hello.conf.py /home/box/web/hello.py:hello_app
+sudo gunicorn -c hello.conf.py /home/box/web/hello.py:hello_app
+#sudo /etc/init.d/gunicorn -c hello.conf.py /home/box/web/hello.py:hello_app
 #sudo /etc/init.d/mysql start﻿
