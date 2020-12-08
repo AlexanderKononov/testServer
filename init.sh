94a13ea765319b4636300334b9782c98857c7240
AlexanderKononov <kononov.nov@mail.ru>
2020-12-09 04:47:56 +0700

    gunicorn setting

--- init.sh
+++ init.sh
@@ -4,8 +4,9 @@ cp -r ./* /home/box/web/
 
 #sudo ln -sf /home/box/web/etc/test.conf /etc/nginx/sites-enabled/default
 #sudo /etc/init.d/nginx restart
-sudo ln -s /home/box/web/etc/test.conf /etc/nginx/sites-enabled/test.conf
+sudo rm /etc/nginx/sites-enabled/default
+sudo ln -sf /home/box/web/etc/test.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
-sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
-sudo /etc/init.d/gunicorn restart
+sudo ln -sf /home/box/web/etc/hello.conf.py /etc/gunicorn.d/hello.conf.py
+sudo /etc/init.d/gunicorn -c hello.conf.py /home/box/web/hello.py:hello_app
 #sudo /etc/init.d/mysql start﻿
