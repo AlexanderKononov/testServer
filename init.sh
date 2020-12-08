e98c9c42eff8b37ee0300acc363affe69db89696
AlexanderKononov <kononov.nov@mail.ru>
2020-12-09 05:03:20 +0700

    stay default nginx

--- init.sh
+++ init.sh
@@ -4,10 +4,9 @@ cp -r ./* /home/box/web/
 
 #sudo ln -sf /home/box/web/etc/test.conf /etc/nginx/sites-enabled/default
 #sudo /etc/init.d/nginx restart
-sudo rm /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/test.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/etc/hello.conf.py /etc/gunicorn.d/hello.conf.py
-sudo gunicorn -c hello.conf.py /home/box/web/hello.py:hello_app
+sudo gunicorn -c /etc/gunicorn.d/hello.conf.py /home/box/web/hello.py:hello_app
 #sudo /etc/init.d/gunicorn -c hello.conf.py /home/box/web/hello.py:hello_app
-#sudo /etc/init.d/mysql start﻿
+#sudo /etc/init.d/mysql start
