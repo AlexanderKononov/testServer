5b32b54c1eba9a6ab618ebdf4eb10ea46f2367d6
AlexanderKononov <kononov.nov@mail.ru>
2020-12-09 06:55:29 +0700

    fix wsgi 2

--- hello.py
+++ hello.py
@@ -1,5 +1,7 @@
 def hello_app(environ, start_response):
-    body = [bytes(i + '\n', 'ascii') for i in environ['QUERY_STRING'].split('&')]
+    body = environ['QUERY_STRING'].replace('&', '\n')
+    body = bytes(body, 'ascii')
+    #body = [bytes(i + '\n', 'ascii') for i in environ['QUERY_STRING'].split('&')]
     status = '200 OK'
     response_headers = [
         ('Content-type', 'text/plain'),
