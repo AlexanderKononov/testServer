87c11c36c563b0d7cabf5975ce9a01d17abce73a
AlexanderKononov <kononov.nov@mail.ru>
2020-12-09 05:23:01 +0700

    gunic as comand

--- init.sh
+++ init.sh
@@ -7,6 +7,8 @@ cp -r ./* /home/box/web/
 sudo ln -sf /home/box/web/etc/test.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/etc/hello.conf.py /etc/gunicorn.d/hello.conf.py
-sudo gunicorn -c /etc/gunicorn.d/hello.conf.py /home/box/web/hello.py:hello_app
+cd /home/box/web/
+sudo gunicorn --bind='0.0.0.0:8080' hello.py:hello_app
+#sudo gunicorn -c /etc/gunicorn.d/hello.conf.py /home/box/web/hello.py:hello_app
 #sudo /etc/init.d/gunicorn -c hello.conf.py /home/box/web/hello.py:hello_app
 #sudo /etc/init.d/mysql start
