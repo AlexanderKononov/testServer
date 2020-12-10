2c97817c3e5f3612e48f24f8090ab56458f1540a
AlexanderKononov <kononov.nov@mail.ru>
2020-12-11 02:55:59 +0700

    del flag -D

--- init.sh
+++ init.sh
@@ -7,10 +7,10 @@ sudo ln -sf /home/box/web/etc/test.conf /etc/nginx/sites-enabled/default
 #sudo ln -sf /home/box/web/etc/test.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 #sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
-cd /home/box/web/
-gunicorn -w 2 -D -c /home/box/web/etc/hello_conf.py hello:app
 cd /home/box/web/ask/
-gunicorn -w 2 -D -c /home/box/web/etc/ask_conf.py ask.wsgi:application
+gunicorn -w 2 -c /home/box/web/etc/ask_conf.py ask.wsgi:application
+cd /home/box/web/
+gunicorn -w 2 -c /home/box/web/etc/hello_conf.py hello:app
 #sudo gunicorn --bind='0.0.0.0:8080' -D hello:hello_app
 #sudo gunicorn -c /etc/gunicorn.d/hello.conf.py /home/box/web/hello.py:hello_app
 #sudo /etc/init.d/gunicorn restart hello:hello_app
