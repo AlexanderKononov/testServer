2c78a9a6ac7b536f36531e85dc0fb44869a3a80e
AlexanderKononov <kononov.nov@mail.ru>
2020-12-10 19:49:51 +0700

    rewrite from othe git

---
+++ etc/hello_conf.py
@@ -0,0 +1,3 @@
+pythonpath = '/home/box/web/'
+bind = "0.0.0.0:8080"
+workers = 4
--- hello.py
+++ hello.py
@@ -1,10 +1,4 @@
 def hello_app(environ, start_response):
-    body=environ['QUERY_STRING'].replace('?','')
-    body=body.split('&')
-    body = [bytes(i + '\n') for i in body]
-    status = '200 OK'
-    response_headers = [
-        ('Content-type', 'text/plain')
-    ]
-    start_response(status, response_headers)
-    return body
+    start_response('200 OK', [('Content-Type', 'text/plain')])
+    return [bytes('\r\n'.join(environ['QUERY_STRING'].split('&')),
+                  encoding="utf8")]
