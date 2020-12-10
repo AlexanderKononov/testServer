81ae8e142c159d814352b1715c8a6a7ba0d612de
AlexanderKononov <kononov.nov@mail.ru>
2020-12-10 20:07:07 +0700

    fix app name

--- init.sh
+++ init.sh
@@ -8,7 +8,7 @@ sudo ln -sf /home/box/web/etc/test.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
 cd /home/box/web/
-gunicorn -w 2 -c /home/box/web/etc/hello_conf.py hello:app
+gunicorn -w 2 -c /home/box/web/etc/hello_conf.py -D hello:hello_app
 #sudo gunicorn --bind='0.0.0.0:8080' -D hello:hello_app
 #sudo gunicorn -c /etc/gunicorn.d/hello.conf.py /home/box/web/hello.py:hello_app
 #sudo /etc/init.d/gunicorn restart hello:hello_app
