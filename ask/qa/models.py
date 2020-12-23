53e3a2beb165d0d42edfbf65fc9b5930ac4c5f96
AlexanderKononov <kononov.nov@mail.ru>
2020-12-23 07:12:52 +0700

    fix user orm

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -8,13 +8,13 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Question (models.Model):
-	#objects = QuestionManager()
+	objects = QuestionManager()
 	title = models.CharField()
 	text = models.TextField()
 	added_at = models.DateTimeField(blank = True, auto_now_add=True)
 	rating = models.IntegerField(default=0)
-	author = models.ForeignKey('User')
-	likes = models.ManyToManyField('User', related_name='likes_set')
+	author = models.ForeignKey(User)
+	likes = models.ManyToManyField(User, related_name='likes_set')
 	
 	def __unicode__(self):
 		return self.title
@@ -34,7 +34,7 @@ class Answer (models.Model):
 	text = models.TextField()
 	added_at = models.DateTimeField(blank = True, auto_now_add=True)
 	question = models.ForeignKey('Question')
-	author = models.ForeignKey('User')
+	author = models.ForeignKey(User)
 	class Meta:
 		db_table = 'qa_answers'
 		ordering = ['-creation_data']
